67571d0d23544522047fd5f421752870180139c8
Balu Manzano <jorgemanzanoweb@gmail.com>
2021-05-28 16:38:16 -0300

    Added manual test to the Wallet connection and balance from token

--- src/containers/HomeContainer/HomeContainer.tsx
+++ src/containers/HomeContainer/HomeContainer.tsx
@@ -1,16 +1,41 @@
 import React, { useEffect } from 'react';
 import { Button } from 'decentraland-ui';
 import FullPageContainer from 'components/FullPageContainer';
+import { ethers } from "ethers";
+import ERC20ABI from 'core/abi/erc20.json';
+
 /**
  * Homepage container, uses the DataHook to recieve always data.
  */
 const HomeContainer = (): JSX.Element => {
-  
-  const startApp = () => {
-    if(window.ethereum) {
-      
+  useEffect(()=>{
+    const setEnable = async ()=>{
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const signer = provider.getSigner();
+      const address = await signer.getAddress();
+
+    
+      console.log(address," signer")
+      const balance = await provider.getBalance(address);
+      console.log(ethers.utils.formatEther(balance), 'balance');
+
+      const ADDRESS_TOKEN = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+      const contract = new ethers.Contract(ADDRESS_TOKEN, ERC20ABI.abi, provider)
+      const balanceToken = await contract.balanceOf(address);
+      // transform the number to decimal representation
+      console.log(balanceToken.toNumber(), ' DUMMY TOKEN');
+    };
+    if(typeof window.ethereum !== 'undefined' && window.ethereum.isMetaMask) {
+      setEnable()
+     
     }
-  }
+  },[]);
+
+  // const startApp = () => {
+  //   if(window.ethereum) {
+      
+  //   }
+  // }
   return (
     <>
       <FullPageContainer>
@@ -18,7 +43,7 @@ const HomeContainer = (): JSX.Element => {
           <h1>
             HELLO WORLD
           </h1>
-          <Button primary onClick={startApp}>Login</Button>
+          <Button primary>Login</Button>
         </div>
       </FullPageContainer>
 
--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,7 @@ module.exports = {
       components: path.resolve(__dirname, 'src/components/'),
       assets: path.resolve(__dirname, 'src/assets/'),
       utils: path.resolve(__dirname, 'src/utils/'),
+      core: path.resolve(__dirname, 'src/core/'),
     },
   },
   module: {
